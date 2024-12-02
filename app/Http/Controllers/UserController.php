c01c1cbd657a77a362f26de3cc1eb34e49e7d118
agustin26l <agustin.lopez2606@gmail.com>
2024-12-02 16:11:35 -0500

    ci: Change the configuration in routes with CSRF

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -16,68 +17,107 @@ class UserController extends Controller
     {
         $usuarios = User::all();
         $data = [
-            'users'=>$usuarios,
-            'status'=> 200,
+            'users' => $usuarios,
+            'status' => 200,
         ];
 
         return response()->json($data, 200);
-   
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function login(Request $request)
-    {
-        
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        $validacion = Validator::make($request->all(),[
+        $validacion = Validator::make($request->all(), [
             'username' => 'required',
             'email' => 'required|email',
             'password' => 'required',
             'profile_picture' => 'required',
-            'bio' => 'required', 
+            'bio' => 'required',
             'is_owner' => 'required|boolean'
-           
+
         ]);
 
         if ($validacion->fails()) {
             return response()->json([
-                'mensaje'=>'Error en la validacion de datos',
-                'error'=> $validacion->errors(),
-                'status'=> 400
-            ]);
+                'mensaje' => 'Error en la validacion de datos',
+                'error' => $validacion->errors(),
+                'status' => 400
+            ], 400);
         }
-        
+
         $user = User::create([
             'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'profile_picture' => $request->profile_picture,
-            'bio'=> $request->bio,
+            'bio' => $request->bio,
             'is_owner' => $request->is_owner
         ]);
-        
-        return response()->json([
-            'mensaje'=>'Usuario creado con exito',
-            'user' => $user,
-            'status'=> 200
-        ], 200);     
 
+        $payload = [
+            'iss' => "airscape_user",
+            'sub' => $user->user_id,
+            'iat' => time(),
+            'exp' => time() + 60 * 60,  // 1 hora
+            'is_owner' => $user->is_owner
+        ];
+
+        $jwt = JWT::encode($payload, env('JWT_SECRET'), 'HS256');
+
+        return response()->json([
+            'mensaje' => 'Usuario creado con exito',
+            'user' => $user,
+            'token' => $jwt,
+            'status' => 200
+        ], 200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function checkUsername($username)
     {
-        //
+        $user = User::where('username', $username)->first();
+
+        if (!$user) {
+            $data = [
+                'mensaje' => 'Usuario not found',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $data = [
+            'user' => $user,
+            'status' => 200,
+            'message' => 'User exists'
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    public function checkEmail($email)
+    {
+        $email = User::where('email', $email)->first();
+
+        if (!$email) {
+            $data = [
+                'mensaje' => 'Email not found',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $data = [
+            'user' => $email,
+            'status' => 200,
+            'message' => 'User exists'
+        ];
+
+        return response()->json($data, 200);
     }
 
     /**
@@ -85,16 +125,16 @@ class UserController extends Controller
      */
     public function edit(Request $request, string $id)
     {
-        
+
         $validacion = Validator::make($request->all(), [
             'username' => 'required',
             'email' => 'required|email',
             'password' => 'required',
             'profile_picture' => 'required',
-            'bio' => 'required', 
+            'bio' => 'required',
             'is_owner' => 'required|boolean'
         ]);
-    
+
         if ($validacion->fails()) {
             return response()->json([
                 'mensaje' => 'Error en la validación de datos',
@@ -103,34 +143,34 @@ class UserController extends Controller
             ]);
         }
         $user = User::find($id);
-    
+
         if (!$user) {
             return response()->json([
                 'mensaje' => 'Usuario no encontrado',
                 'status' => 404
             ]);
         }
-    
+
         $user->username = $request->username;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
         $user->profile_picture = $request->profile_picture;
         $user->bio = $request->bio;
         $user->is_owner = $request->is_owner;
-    
+
         if (!$user->save()) {
             return response()->json([
                 'mensaje' => 'No se pudo actualizar el usuario',
                 'status' => 500
             ]);
         }
-    
+
         return response()->json([
             'mensaje' => 'Usuario actualizado correctamente',
             'status' => 200
         ]);
     }
-    
+
 
     /**
      * Update the specified resource in storage.
@@ -145,27 +185,26 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-       
+
         $user = User::find($id);
-    
+
         if (!$user) {
             return response()->json([
                 'mensaje' => 'Usuario no encontrado',
                 'status' => 404
             ]);
         }
-    
+
         if (!$user->delete()) {
             return response()->json([
                 'mensaje' => 'No se pudo eliminar el usuario',
                 'status' => 500
             ]);
         }
-    
+
         return response()->json([
             'mensaje' => 'Usuario eliminado correctamente',
             'status' => 200
         ]);
     }
-    
 }
--- routes/web.php
+++ routes/web.php
@@ -3,31 +3,36 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ListingsController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthenticateJWT;
 use Illuminate\Support\Facades\Route;
 
 // Public Routes
 // Users
-
-Route::get('/user', [UserController::class, 'index']);
 Route::post('/user', [UserController::class, 'store']);
 
 // Listings
-
 Route::get('/listings', [ListingsController::class, 'index']);
 
 // Authentication
-
 Route::post('/login', [AuthController::class, 'login']);
 
 // Private routes
-
-Route::middleware('auth.jwt')->group(function () {
-    // Users
-
+Route::middleware([AuthenticateJWT::class])->group(function () {
+    // Show all users
+    Route::get('/user', [UserController::class, 'index']);
+    
     Route::put('/user/{id}', [UserController::class, 'edit']);
+
     Route::delete('/user/{id}', [UserController::class, 'destroy']);
+    
+    // Check username user
+    Route::get('/check-username/{username}', [UserController::class, 'checkUsername']);
+    // Check email user
+
+    Route::get('/check-email/{email}', [UserController::class, 'checkEmail']);
 
     // Listings
     Route::post('/listings', [ListingsController::class, 'store']);
 });
+
 
