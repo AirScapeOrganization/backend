60b9d923c028bcf618c9daf620ac295970840fa6
keiner12345 <gzkeiner@gmail.com>
2024-12-03 14:10:30 -0500

    Feat: Poner url para las fotos de propiedades listo

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -6,9 +6,32 @@ use App\Models\Listings;
 use App\Models\Photos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class ListingsController extends Controller
 {
+    public function index()
+    {
+        $listings = Listings::all();
+
+        $data = [
+            'properties' => $listings,
+            'status' => 200,
+        ];
+
+        if ($listings->isEmpty()) {
+            return response()->json(['message' => 'No listings found'], 404);
+        }
+
+        return response()->json($data, 200);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+
     public function store(Request $request)
     {
         // Validación de los datos de entrada
@@ -45,6 +68,7 @@ class ListingsController extends Controller
         $newListing->num_bathrooms = $request->num_bathrooms;
         $newListing->max_guests = $request->max_guests;
         $newListing->user_id = $request->user_id ?? 1;  // valor por defecto si no se pasa user_id
+        $newListing->created_at = now();
 
         if (!$newListing->save()) {
             return response()->json([
@@ -53,24 +77,104 @@ class ListingsController extends Controller
             ]);
         }
 
-        // Guardar la foto en la tabla Photos
+        // Crear la foto al nuevo listing
         $photo = new Photos();
-        $photo->listing_id = $newListing->listing_id;
-        $photo->photo_url = $request->photo_url; // Asumiendo que la URL de la foto se envía como parte de la solicitud
+        $photo->listing_id = $newListing->listing_id;  
+        $photo->photo_url = $request->photo_url;
+        $photo->created_at = now();
+        $photo->save();  
 
-        if (!$photo->save()) {
+        if (!$photo) {
             return response()->json([
                 'mensaje' => 'No se pudo guardar la foto',
                 'status' => 500
             ]);
         }
 
-        // Retornar respuesta exitosa
+        
         return response()->json([
-            'mensaje' => 'Propiedad y foto creada correctamente',
+            'mensaje' => 'Propiedad creada correctamente',
             'listing' => $newListing,
             'photo' => $photo,
             'status' => 201
         ]);
+    }
+
+
+
+
+
+    /**
+     * Display the specified resource.
+     */
+   
+    public function edit(Request $request, string $id)
+    {
+        $listings = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'address' => 'required|string',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+            'price_per_night' => 'required|numeric',
+            'num_bedrooms' => 'required|integer',
+            'num_bathrooms' => 'required|integer',
+            'max_guests' => 'required|integer',
+        ]);
+    
+        if ($listings->fails()) {
+            return response()->json([
+                'mensaje' => 'Error en la validación de datos',
+                'error' => $listings->errors(),
+                'status' => 400
+            ]);
+        }
+        $listings = Listings::find($id);
+    
+        if (!$listings) {
+            return response()->json([
+                'mensaje' => 'Propiedad no encontrada',
+                'status' => 404
+            ]);
+        }
+        $listings->title = $request->title;
+        $listings->description = $request->description;
+        $listings->address = $request->address;
+        $listings->latitude = $request->latitude;
+        $listings->longitude = $request->longitude;
+        $listings->price_per_night = $request->price_per_night;
+        $listings->num_bedrooms = $request->num_bedrooms;
+        $listings->num_bathrooms = $request->num_bathrooms;
+        $listings->max_guests = $request->max_guests;
+    
+        if (!$listings->save()) {
+            return response()->json([
+                'mensaje' => 'No se pudo actualizar la propiedad',
+                'status' => 500
+            ]);
+        }
+    
+        return response()->json([
+            'mensaje' => 'Propiedad actualizada correctamente',
+            'status' => 200
+        ]);
+    }
+       
+    
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
